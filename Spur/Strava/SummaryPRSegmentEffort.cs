c3d8f55463b56372e1c10a9bbcfd8c991135c052
Vegard Løkken <vegard@loekken.org>
2023-10-08 21:27:47 +0200

    Fix formatting

--- Spur/Strava/SummaryPRSegmentEffort.cs
+++ Spur/Strava/SummaryPRSegmentEffort.cs
@@ -7,17 +7,17 @@ public class SummaryPRSegmentEffort
 {
     [JsonPropertyName("pr_activity_id")]
     // The unique identifier of the activity related to the PR effort.
-    public long Pr_activity_id {get;set;}
+    public long Pr_activity_id { get; set; }
 
     [JsonPropertyName("pr_elapsed_time")]
     // The elapsed time ot the PR effort.
-    public int Pr_elapsed_time {get;set;}
+    public int Pr_elapsed_time { get; set; }
 
     [JsonPropertyName("pr_date")]
     // The time at which the PR effort was started.
-    public DateTimeOffset Pr_date {get;set;}
+    public DateTimeOffset Pr_date { get; set; }
 
     [JsonPropertyName("effort_count")]
     // Number of efforts by the authenticated athlete on this segment.
-    public int Effort_count {get;set;}
+    public int Effort_count { get; set; }
 }
--- Spur/Strava/SummarySegmentEffort.cs
+++ Spur/Strava/SummarySegmentEffort.cs
@@ -7,29 +7,29 @@ public class SummarySegmentEffort
 {
     [JsonPropertyName("id")]
     // The unique identifier of this effort
-    public long Id {get;set;}
+    public long Id { get; set; }
 
     [JsonPropertyName("activity_id")]
     // The unique identifier of the activity related to this effort
-    public long ActivityId {get;set;}
+    public long ActivityId { get; set; }
 
     [JsonPropertyName("elapsed_time")]
     // The effort's elapsed time
-    public int ElapsedTime {get;set;}
+    public int ElapsedTime { get; set; }
 
     [JsonPropertyName("start_date")]
     // The time at which the effort was started.
-    public DateTimeOffset StartDate {get;set;}
+    public DateTimeOffset StartDate { get; set; }
 
     [JsonPropertyName("start_date_local")]
     // The time at which the effort was started in the local timezone.
-    public DateTimeOffset StartDateLocal {get;set;}
+    public DateTimeOffset StartDateLocal { get; set; }
 
     [JsonPropertyName("distance")]
     // The effort's distance in meters
-    public float Distance {get;set;}
+    public float Distance { get; set; }
 
     [JsonPropertyName("is_kom")]
     // Whether this effort is the current best on the leaderboard
-    public bool IsKom {get;set;}
+    public bool IsKom { get; set; }
 }
