58453e8bac9376298ea3046d9c53b5fd927278c0
Vegard Løkken <vegard@loekken.org>
2023-11-14 13:26:30 +0100

    Implement infinite scrolling in activity feed

---
+++ Spur/wwwroot/infinite-scrolling-marker.js
@@ -0,0 +1,41 @@
+export function initialize(loadMoreEl, componentRef) {
+    const options = {
+        root: findClosestScrollingContainer(loadMoreEl),
+        rootMargin: "0px",
+        threshold: 0,
+    };
+
+    const observer = new IntersectionObserver(async (entries) => {
+        for (let entry of entries) {
+            if (entry.isIntersecting) {
+                observer.unobserve(loadMoreEl);
+                await componentRef.invokeMethodAsync("OnMarkerVisible");
+            }
+        }
+    }, options);
+
+    observer.observe(loadMoreEl);
+
+    return {
+        onNewItems: () => {
+            observer.unobserve(loadMoreEl);
+            observer.observe(loadMoreEl);
+        },
+        dispose: () => {
+            observer.disconnect();
+        }
+    }
+}
+
+function findClosestScrollingContainer(el) {
+    while (el != null)
+    {
+        const style = getComputedStyle(el);
+        if (style.overflowY !== "visible")
+        {
+            return el;
+        }
+        el = el.parentElement;
+    }
+    return null;
+}
