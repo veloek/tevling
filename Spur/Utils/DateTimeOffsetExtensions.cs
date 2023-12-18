4433b8d484d10ed19572c804e699c5d4312d4365
Vegard Løkken <vegard@loekken.org>
2023-12-18 09:27:07 +0100

    Add additional category for last weeks activities
    Also converting DateTimeOffsets to UTC before comparing them with
    `UtcNow` to make sure we're comparing apples to apples.

--- Spur/Utils/DateTimeOffsetExtensions.cs
+++ Spur/Utils/DateTimeOffsetExtensions.cs
@@ -4,36 +4,46 @@ public static class DateTimeOffsetExt
 {
     public static bool IsSameDateUtc(this DateTimeOffset dto)
     {
-        return dto.Date == DateTimeOffset.UtcNow.Date;
+        return dto.UtcDateTime.Date == DateTimeOffset.UtcNow.Date;
     }
 
     public static bool IsYesterdayUtc(this DateTimeOffset dto)
     {
-        return dto.Date == DateTimeOffset.UtcNow.Date.AddDays(-1);
+        return dto.UtcDateTime.Date == DateTimeOffset.UtcNow.Date.AddDays(-1);
     }
 
     public static bool IsSameWeekStartingOnMondayUtc(this DateTimeOffset dto)
     {
-        return (DateTimeOffset.UtcNow - dto) <= TimeSpan.FromDays(7)
-            && (dto.DayOfWeek == DayOfWeek.Sunday
-                ? DateTimeOffset.UtcNow.DayOfWeek == DayOfWeek.Sunday
-                : dto.DayOfWeek <= DateTimeOffset.UtcNow.DayOfWeek);
+        TimeSpan timeSinceMonday = DateTimeOffset.UtcNow.DayOfWeek == DayOfWeek.Sunday
+            ? TimeSpan.FromDays(6)
+            : TimeSpan.FromDays((int)DateTimeOffset.UtcNow.DayOfWeek - 1);
+
+        return (DateTimeOffset.UtcNow.Date - dto.UtcDateTime.Date) <= timeSinceMonday;
+    }
+
+    public static bool IsLastWeekStartingOnMondayUtc(this DateTimeOffset dto)
+    {
+        TimeSpan timeSinceLastMonday = DateTimeOffset.UtcNow.DayOfWeek == DayOfWeek.Sunday
+            ? TimeSpan.FromDays(6 + 7)
+            : TimeSpan.FromDays((int)DateTimeOffset.UtcNow.DayOfWeek - 1 + 7);
+
+        return (DateTimeOffset.UtcNow.Date - dto.UtcDateTime.Date) <= timeSinceLastMonday;
     }
 
     public static bool IsSameMonthUtc(this DateTimeOffset dto)
     {
-        return dto.Year == DateTimeOffset.UtcNow.Year
-            && dto.Month == DateTimeOffset.UtcNow.Month;
+        return dto.UtcDateTime.Year == DateTimeOffset.UtcNow.Year
+            && dto.UtcDateTime.Month == DateTimeOffset.UtcNow.Month;
     }
 
     public static bool IsLastMonthUtc(this DateTimeOffset dto)
     {
-        return dto.Year == DateTimeOffset.UtcNow.AddMonths(-1).Year
-            && dto.Month == DateTimeOffset.UtcNow.AddMonths(-1).Month;
+        return dto.UtcDateTime.Year == DateTimeOffset.UtcNow.AddMonths(-1).Year
+            && dto.UtcDateTime.Month == DateTimeOffset.UtcNow.AddMonths(-1).Month;
     }
 
     public static bool IsSameYearUtc(this DateTimeOffset dto)
     {
-        return dto.Year == DateTimeOffset.UtcNow.Year;
+        return dto.UtcDateTime.Year == DateTimeOffset.UtcNow.Year;
     }
 }
