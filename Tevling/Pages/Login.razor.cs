d09f90cd98c80952d6e606bbce40f891a9970495
Vegard Løkken <vegard@loekken.org>
2024-04-24 21:32:32 +0200

    Fix exception thrown on login page
    When the URL was missing a query parameter `returnUrl`, the call to
    `Uri.EscapeDataString` would throw. It turns out
    `SupplyParameterFromQueryAttribute` sets the property to `null` if the
    query parameter is missing, which defeats the purpose of initializing
    the property to `string.Empty`.

--- Tevling/Pages/Login.razor.cs
+++ Tevling/Pages/Login.razor.cs
@@ -5,7 +5,7 @@ public partial class Login : ComponentBase
     [Inject] private StravaConfig StravaConfig { get; set; } = null!;
 
     [SupplyParameterFromQuery(Name = "returnUrl")]
-    private string ReturnUrl { get; set; } = string.Empty;
+    private string? ReturnUrl { get; set; }
 
-    private string RedirectUri => $"{StravaConfig.RedirectUri}?returnUrl={Uri.EscapeDataString(ReturnUrl)}";
+    private string RedirectUri => $"{StravaConfig.RedirectUri}?returnUrl={Uri.EscapeDataString(ReturnUrl ?? string.Empty)}";
 }
