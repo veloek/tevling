6f694c59700853411d0ce0fdda4d4e21ae9962f0
Vegard Løkken <vegard@loekken.org>
2024-11-25 16:47:04 +0100

    Add migration for Athlete's Created timestamp
    This should've been a part of commit 6f0c15c4.

---
+++ Tevling/Migrations/20241125154623_AthleteCreatedTimestamp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tevling.Migrations
+{
+    /// <inheritdoc />
+    public partial class AthleteCreatedTimestamp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Created",
+                table: "Athletes",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "Created",
+                table: "Athletes",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "INTEGER");
+        }
+    }
+}
