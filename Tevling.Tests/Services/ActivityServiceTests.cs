c355fca8c0010c2c838082239440cf853e906ceb
Vegard Løkken <vegard@loekken.org>
2025-01-24 19:32:43 +0100

    Add skeleton for some more service tests

---
+++ Tevling.Tests/Services/ActivityServiceTests.cs
@@ -0,0 +1,55 @@
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tevling.Services;
+
+public class ActivityServiceTests
+{
+    [Fact]
+    public async Task CreateActivityAsync_should_return_existing_activity_if_it_exists()
+    {
+
+    }
+
+    [Fact]
+    public async Task CreateActivityAsync_should_publish_activity_feed_update()
+    {
+
+    }
+
+    [Fact]
+    public async Task UpdateActivityAsync_should_publish_activity_feed_update()
+    {
+
+    }
+
+    [Fact]
+    public async Task DeleteActivityAsync_should_publish_activity_feed_update()
+    {
+
+    }
+
+    [Fact]
+    public async Task GetActivitiesAsync_should_return_include_following_activities()
+    {
+
+    }
+
+    [Fact]
+    public async Task GetActivitiesAsync_should_support_pagination()
+    {
+
+    }
+
+    [Fact]
+    public async Task GetActivityFeedForAthlete_should_return_activities_for_athlete_and_following()
+    {
+
+    }
+
+    [Fact]
+    public async Task ImportActivitiesForAthleteAsync_should_create_new_and_update_existing_activites()
+    {
+
+    }
+}
---
+++ Tevling.Tests/Services/ChallengeServiceTests.cs
@@ -0,0 +1,37 @@
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tevling.Services;
+
+public class ChallengeServiceTests
+{
+    [Fact]
+    public async Task GetChallengesAsync_should_return_only_joined_challenges()
+    {
+
+    }
+
+    [Fact]
+    public async Task GetChallengesAsync_should_not_return_private_challenges()
+    {
+
+    }
+
+    [Fact]
+    public async Task GetChallengesAsync_should_return_private_challenges_if_invited()
+    {
+
+    }
+
+    [Fact]
+    public async Task GetChallengesAsync_should_return_authored_and_joined_challenges()
+    {
+
+    }
+
+    [Fact]
+    public async Task GetChallengesAsync_should_search_challenges_by_title()
+    {
+
+    }
+}
