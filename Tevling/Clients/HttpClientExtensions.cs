563cd163ca224c22b30ad4dfc5fc569bd53e71ca
Vegard Løkken <vegard@loekken.org>
2025-03-07 13:54:13 +0100

    Follow MS docs and configure handler lifetime

--- Tevling/Clients/HttpClientExtensions.cs
+++ Tevling/Clients/HttpClientExtensions.cs
@@ -8,6 +8,8 @@ public static class HttpClientExtensions
     public static IHttpClientBuilder AddStravaClient(this IServiceCollection services)
         => services
             .AddHttpClient<IStravaClient, StravaClient>()
+            // https://learn.microsoft.com/en-us/dotnet/architecture/microservices/implement-resilient-applications/implement-http-call-retries-exponential-backoff-polly
+            .SetHandlerLifetime(TimeSpan.FromMinutes(5))
             .AddPolicyHandler(
                 HttpPolicyExtensions
                     .HandleTransientHttpError()
