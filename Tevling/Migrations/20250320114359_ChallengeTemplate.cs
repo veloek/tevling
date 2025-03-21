e7fbb7404d93d8378baa9ca1a521345d11430663
ChrJohansen <christian766@gmail.com>
2025-03-21 14:54:59 +0100

    DB migration: Add ChallengeTemplate

---
+++ Tevling/Migrations/20250320114359_ChallengeTemplate.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tevling.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChallengeTemplate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChallengeTemplates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Measurement = table.Column<int>(type: "INTEGER", nullable: false),
+                    ActivityTypes = table.Column<string>(type: "TEXT", nullable: false),
+                    Created = table.Column<long>(type: "INTEGER", nullable: false),
+                    IsPrivate = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CreatedById = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChallengeTemplates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ChallengeTemplates_Athletes_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "Athletes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChallengeTemplates_CreatedById",
+                table: "ChallengeTemplates",
+                column: "CreatedById");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChallengeTemplates");
+        }
+    }
+}
---
+++ Tevling/Model/ChallengeTemplate.cs
@@ -0,0 +1,17 @@
+using Tevling.Strava;
+
+namespace Tevling.Model;
+
+public class ChallengeTemplate
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public ChallengeMeasurement Measurement { get; set; }
+    public IList<ActivityType> ActivityTypes { get; set; } = [];
+    public DateTimeOffset Created { get; set; }
+    public bool IsPrivate { get; set; }
+
+    public int CreatedById { get; set; }
+    public Athlete? CreatedBy { get; set; }
+}
