62d4bf01e24ccbb9334239a1bb3d08d353790f48
Vegard Løkken <vegard@loekken.org>
2025-05-13 14:08:40 +0200

    Add migration to fix ChallengeGroup schema

---
+++ Tevling/Migrations/20250513120637_FixChallengeGroup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tevling.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixChallengeGroup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Created",
+                table: "ChallengeGroups",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "Created",
+                table: "ChallengeGroups",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "INTEGER");
+        }
+    }
+}
