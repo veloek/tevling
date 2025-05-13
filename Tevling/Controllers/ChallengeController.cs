231553ece8f6da05113015fb370be3c8a3e12f45
Christian Johansen <christian766@gmail.com>
2025-05-13 14:54:54 +0200

    Challenge-api WIP

---
+++ Tevling/Controllers/ChallengeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.FeatureManagement.Mvc;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace Tevling.Controllers;
+
+[ApiController]
+[Route("challenges")]
+[FeatureGate(FeatureFlag.EnableChallengeApi)]
+public class ChallengeController : ControllerBase
+{
+    public ChallengeController()
+    {
+    }
+
+    [HttpGet]
+    [Route("scoreboard/{stravaId}")]
+    public IActionResult GetChallenges(long stravaId)
+    {
+        List<AthleteScore> scores =
+        [
+            new AthleteScore(
+                "Fjomp1",
+               "23"
+            ),
+
+            new AthleteScore(
+                "Fjomp2",
+                "30"
+            ),
+
+        ];
+        ScoreBoard scoreBoard = new
+        (
+            scores
+        );
+        return new JsonResult(scoreBoard);
+    }
+}
