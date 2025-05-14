cc383b13e5832003a6734706474a63a2fe8c3255
ChrJohansen <christian766@gmail.com>
2025-05-14 08:26:39 +0200

    Get challenges from db

--- Tevling/Controllers/ChallengeController.cs
+++ Tevling/Controllers/ChallengeController.cs
@@ -7,33 +7,33 @@ namespace Tevling.Controllers;
 [ApiController]
 [Route("challenges")]
 [FeatureGate(FeatureFlag.EnableChallengeApi)]
-public class ChallengeController : ControllerBase
+public class ChallengeController(IChallengeService challengeService) : ControllerBase
 {
-    public ChallengeController()
-    {
-    }
-
     [HttpGet]
     [Route("scoreboard/{stravaId}")]
-    public IActionResult GetChallenges(long stravaId)
+    public async Task<IActionResult> GetChallenges(int stravaId, CancellationToken ct = default)
     {
-        List<AthleteScore> scores =
-        [
-            new AthleteScore(
-                "Fjomp1",
-               "23"
-            ),
-
-            new AthleteScore(
-                "Fjomp2",
-                "30"
-            ),
-
-        ];
-        ScoreBoard scoreBoard = new
-        (
-            scores
+        ChallengeFilter filter = new(
+            string.Empty,
+            stravaId,
+            OnlyJoinedChallenges: true,
+            IncludeOutdatedChallenges: false
         );
-        return new JsonResult(scoreBoard);
+
+        Challenge[] challenges = await challengeService.GetChallengesAsync(stravaId, filter, null, ct);
+
+        var output = await Task.WhenAll(
+            challenges.Select(async c =>
+            {
+                ScoreBoard scoreboard = await challengeService.GetScoreBoardAsync(c.Id, ct);
+                return new
+                {
+                    challengeTitle = c.Title,
+                    scores = scoreboard.Scores,
+                };
+            })
+        );
+
+        return new JsonResult(output);
     }
 }
