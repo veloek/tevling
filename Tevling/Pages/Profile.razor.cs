1f0664816ff89a00af33ccc304d269e07705a462
Vegard Løkken <vegard@loekken.org>
2025-10-08 10:54:48 +0200

    Add translations for the Profile page

--- Tevling/Pages/Profile.razor.cs
+++ Tevling/Pages/Profile.razor.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Localization;
+
 namespace Tevling.Pages;
 
 public partial class Profile : ComponentBase
@@ -6,6 +8,7 @@ public partial class Profile : ComponentBase
     [Inject] private IActivityService ActivityService { get; set; } = null!;
     [Inject] private IAthleteService AthleteService { get; set; } = null!;
     [Inject] private IBrowserTime BrowserTime { get; set; } = null!;
+    [Inject] private IStringLocalizer<Profile> Loc { get; set; } = null!;
 
     private string? CreatedTime;
     private bool Importing { get; set; }
@@ -20,30 +23,30 @@ public partial class Profile : ComponentBase
     protected override async Task OnParametersSetAsync()
     {
         DateTimeOffset browserTime = await BrowserTime.ConvertToLocal(Athlete.Created);
-        CreatedTime = browserTime.ToString("dd'.'MM'.'yyyy");
+        CreatedTime = browserTime.ToString("d");
     }
 
     private async Task Import()
     {
         if (Athlete.HasImportedActivities)
         {
-            ImportResult = "Sorry, you can only import once.";
+            ImportResult = Loc["SorryOnlyOnce"];
             return;
         }
 
         Importing = true;
-        ImportResult = "Importing activities, please wait...";
+        ImportResult = Loc["ImportingActivities"];
         try
         {
             await ActivityService.ImportActivitiesForAthleteAsync(
                 Athlete.Id,
                 DateTimeOffset.Now - TimeSpan.FromDays(30));
             Athlete = await AthleteService.SetHasImportedActivities(Athlete.Id);
-            ImportResult = "Import completed successfully";
+            ImportResult = Loc["ImportSuccessful"];
         }
         catch (Exception ex)
         {
-            ImportResult = $"Import failed with error: {ex.Message}";
+            ImportResult = string.Format(Loc["ImportFailed"], ex.Message);
         }
         finally
         {
