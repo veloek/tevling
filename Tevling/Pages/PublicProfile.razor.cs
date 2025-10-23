774545da013a4f6bb029096cd7a035db06cab2b9
ChrJohansen <christian766@gmail.com>
2025-10-23 13:03:28 +0200

    - Created a JoinedChallengeCard component - View Joined challenges in public profile

--- Tevling/Pages/PublicProfile.razor.cs
+++ Tevling/Pages/PublicProfile.razor.cs
@@ -10,12 +10,15 @@ public partial class PublicProfile : ComponentBase
     [Inject] private IAthleteService AthleteService { get; set; } = null!;
 
     [Inject] private IBrowserTime BrowserTime { get; set; } = null!;
+    [Inject] private IChallengeService ChallengeService { get; set; } = null!;
+    
 
 
     [Parameter] public int AthleteToViewId { get; set; }
     private Athlete Athlete { get; set; } = default!;
     private Athlete AthleteToView { get; set; } = default!;
     private string? CreatedTime;
+    private Dictionary<Challenge, (string, string)> ActiveChallenges { get; } = [];
 
 
     protected override async Task OnInitializedAsync()
@@ -32,6 +35,58 @@ public partial class PublicProfile : ComponentBase
             throw new InvalidOperationException($"Athlete with ID {AthleteToViewId} not found");
         DateTimeOffset browserTime = await BrowserTime.ConvertToLocal(AthleteToView.Created);
         CreatedTime = browserTime.ToString("d");
+        await FetchActiveChallenges();
+    }
+    
+    private async Task FetchActiveChallenges(CancellationToken ct = default)
+    {
+        DateTimeOffset browserTime = await BrowserTime.ConvertToLocal(DateTimeOffset.Now, ct);
+        ChallengeFilter filter = new(
+            string.Empty,
+            null,
+            OnlyJoinedChallenges: true,
+            IncludeOutdatedChallenges: false);
+        Challenge[] challenges = await ChallengeService.GetChallengesAsync(AthleteToView.Id, filter, null, ct);
+
+        foreach (Challenge challenge in challenges)
+        {
+            if (challenge.End <= browserTime) continue;
+
+            ScoreBoard scoreBoard = await ChallengeService.GetScoreBoardAsync(challenge.Id, ct);
+
+            AthleteScore? score = scoreBoard.Scores.FirstOrDefault(x => x.Name == AthleteToView.Name);
+            if (score is null) continue;
+
+            int placement = scoreBoard.Scores.ToList().IndexOf(score) + 1;
+            if (placement is 0) continue;
+
+            string placementString = GetOrdinal(placement);
+
+            ActiveChallenges[challenge] = (placementString, score.Score);
+        }
+    }
+    
+    private string GetOrdinal(int num)
+    {
+        if (num <= 0) return num.ToString();
+
+        return num switch
+        {
+            1 => $"1{Loc["First"]} 🥇",
+            2 => $"2{Loc["Second"]} 🥈",
+            3 => $"3{Loc["Third"]} 🥉",
+            _ => (num % 100) switch
+            {
+                11 or 12 or 13 => num + Loc["Nth"],
+                _ => (num % 10) switch
+                {
+                    1 => num + Loc["First"],
+                    2 => num + Loc["Second"],
+                    3 => num + Loc["Third"],
+                    _ => num + Loc["Nth"],
+                },
+            },
+        };
     }
     
     private async Task ToggleFollowing(int followingId)
