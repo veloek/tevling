7154cb1831f7ac6e04440ff6ea178fef4ad1e938
ChrJohansen <christian766@gmail.com>
2025-10-29 11:07:57 +0100

    Add "medals won" on public profile

--- Tevling/Pages/PublicProfile.razor.cs
+++ Tevling/Pages/PublicProfile.razor.cs
@@ -4,6 +4,24 @@ namespace Tevling.Pages;
 
 public partial class PublicProfile : ComponentBase
 {
+    private record Medals
+    {
+        public int First { get; set; }
+        public int Second { get; set; }
+        public int Third { get; set; }
+
+        public string GetFirst() => First + " 🥇";
+        public string GetSecond() => Second + " 🥈";
+        public string GetThird() => Third + " 🥉";
+
+        public void Reset()
+        {
+            First = 0;
+            Second = 0;
+            Third = 0;
+        }
+    }
+
     [Inject] private IAuthenticationService AuthenticationService { get; set; } = null!;
     [Inject] private IStringLocalizer<PublicProfile> Loc { get; set; } = null!;
 
@@ -11,7 +29,6 @@ public partial class PublicProfile : ComponentBase
 
     [Inject] private IBrowserTime BrowserTime { get; set; } = null!;
     [Inject] private IChallengeService ChallengeService { get; set; } = null!;
-    
 
 
     [Parameter] public int AthleteToViewId { get; set; }
@@ -19,6 +36,7 @@ public partial class PublicProfile : ComponentBase
     private Athlete? AthleteToView { get; set; }
     private string? CreatedTime;
     private Dictionary<string, (string, string)> ActiveChallenges { get; } = [];
+    private Medals AthleteMedals { get; } = new();
 
 
     protected override async Task OnInitializedAsync()
@@ -35,11 +53,45 @@ public partial class PublicProfile : ComponentBase
             DateTimeOffset browserTime = await BrowserTime.ConvertToLocal(AthleteToView!.Created);
             CreatedTime = browserTime.ToString("d");
             ActiveChallenges.Clear();
+            AthleteMedals.Reset();
             await FetchActiveChallenges();
+            await CountMedals();
         }
-        
     }
-    
+
+    private async Task CountMedals(CancellationToken ct = default)
+    {
+        DateTimeOffset browserTime = await BrowserTime.ConvertToLocal(DateTimeOffset.Now, ct);
+        ChallengeFilter filter = new(
+            string.Empty,
+            null,
+            OnlyJoinedChallenges: true,
+            IncludeOutdatedChallenges: true);
+        Challenge[] challenges = await ChallengeService.GetChallengesAsync(AthleteToView!.Id, filter, null, ct);
+        foreach (Challenge challenge in challenges)
+        {
+            if (challenge.End > browserTime) continue;
+            ScoreBoard scoreBoard = await ChallengeService.GetScoreBoardAsync(challenge.Id, ct);
+
+            AthleteScore? score = scoreBoard.Scores.FirstOrDefault(x => x.Name == AthleteToView.Name);
+            if (score is null) continue;
+
+            int placement = scoreBoard.Scores.ToList().IndexOf(score) + 1;
+            switch (placement)
+            {
+                case 1:
+                    AthleteMedals.First += 1;
+                    break;
+                case 2:
+                    AthleteMedals.Second += 1;
+                    break;
+                case 3:
+                    AthleteMedals.Third += 1;
+                    break;
+            }
+        }
+    }
+
     private async Task FetchActiveChallenges(CancellationToken ct = default)
     {
         DateTimeOffset browserTime = await BrowserTime.ConvertToLocal(DateTimeOffset.Now, ct);
@@ -67,7 +119,7 @@ public partial class PublicProfile : ComponentBase
             ActiveChallenges[challenge.Title] = (placementString, score.Score);
         }
     }
-    
+
     private string GetOrdinal(int num)
     {
         if (num <= 0) return num.ToString();
@@ -90,24 +142,24 @@ public partial class PublicProfile : ComponentBase
             },
         };
     }
-    
+
     private async Task ToggleFollowing(int followingId)
     {
         Athlete = await AthleteService.ToggleFollowingAsync(Athlete, followingId);
-        
+
         if (Athlete.Id == AthleteToViewId)
         {
             AthleteToView = await AthleteService.GetAthleteByIdAsync(AthleteToViewId) ??
                 throw new InvalidOperationException($"Athlete with ID {AthleteToViewId} not found");
         }
-        
+
         await InvokeAsync(StateHasChanged);
     }
-    
+
     private async Task RemoveFollower(int followerId)
     {
         Athlete = await AthleteService.RemoveFollowerAsync(Athlete, followerId);
-        
+
         if (Athlete.Id == AthleteToViewId)
         {
             AthleteToView = await AthleteService.GetAthleteByIdAsync(AthleteToViewId) ??
