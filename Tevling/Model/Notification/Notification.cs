6400865bcfb175536c73a2058ac8dd0843510e57
ChrJohansen <christian766@gmail.com>
2025-11-28 13:04:42 +0100

    Remove unused method Message()

--- Tevling/Model/Notification/Notification.cs
+++ Tevling/Model/Notification/Notification.cs
@@ -14,15 +14,4 @@ public class Notification
 
     public DateTimeOffset? Read { get; set; }
     public required NotificationType Type { get; init; }
-
-    public string Message()
-    {
-        return Type switch
-        {
-            NotificationType.FollowRequestCreated => "Wants to follow you!",
-            NotificationType.FollowRequestAccepted => "Accepted your follow request!",
-            NotificationType.ChallengeInvite => $"Invited you to join the \"{ChallengeTitle}\" challenge ",
-            _ => "",
-        };
-    }
 }
