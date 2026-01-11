3b28725c720e9dfbc38731278a34be6e0adf2caa
Vegard Løkken <vegard@loekken.org>
2026-01-11 20:53:51 +0100

    Replace custom caching with framework native
    Using output caching for storing profile pictures on the server and
    response caching for adding cache headers for the client to use.

--- Tevling/Controllers/ProfileController.cs
+++ Tevling/Controllers/ProfileController.cs
@@ -1,100 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
 using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
 
 namespace Tevling.Controllers;
 
 [ApiController]
 [Route("profile")]
-public class ProfileController(
-    ILogger<ProfileController> logger,
-    IAthleteService athleteService,
-    IHttpClientFactory httpClientFactory)
-    : ControllerBase
+public class ProfileController(IAthleteService athleteService, IHttpClientFactory httpClientFactory) : ControllerBase
 {
-    private static readonly string PROFILES_DIR = Path.Join(Environment.CurrentDirectory, "storage", "profiles");
-    private static readonly TimeSpan CACHE_MAXAGE = TimeSpan.FromDays(1);
-    private const string PROFILE_FALLBACK = "/avatar/athlete/large.png";
+    private const int DayInSeconds = 24 * 60 * 60;
+    private const string FallbackPicture = "/avatar/athlete/large.png";
 
     [HttpGet]
     [Route("{athleteId}")]
+    [OutputCache(Duration = DayInSeconds)]
+    [ResponseCache(Duration = DayInSeconds)]
     public async Task<IActionResult> GetProfilePicture([FromRoute] int athleteId)
     {
         Athlete? athlete = await athleteService.GetAthleteByIdAsync(athleteId, HttpContext.RequestAborted);
+
         if (athlete is null)
             return NotFound();
 
-        if (athlete.ImgUrl is null)
-            return LocalRedirect(PROFILE_FALLBACK);
+        if (!Uri.IsWellFormedUriString(athlete.ImgUrl, UriKind.Absolute))
+            return LocalRedirect(FallbackPicture);
 
-        // Check if profile picture is already cached (and not too old)
-        (FileInfo? cachedProfile, string contentType) = GetCachedProfile(athleteId);
-        if (cachedProfile is not null)
-        {
-            TimeSpan age = DateTime.UtcNow - cachedProfile.CreationTimeUtc;
-            if (age < CACHE_MAXAGE)
-            {
-                FileStream profile = cachedProfile.OpenRead();
+        using HttpClient httpClient = httpClientFactory.CreateClient();
+        HttpResponseMessage response = (await httpClient.GetAsync(athlete.ImgUrl, HttpContext.RequestAborted))
+            .EnsureSuccessStatusCode();
 
-                return File(profile, contentType);
-            }
-        }
+        byte[] data = await response.Content.ReadAsByteArrayAsync(HttpContext.RequestAborted);
+        string? contentType = response.Content.Headers.ContentType?.MediaType;
 
-        // Fetch profile picture and store it
-        logger.LogDebug("Fetching profile picture for athlete: {AthleteId}", athleteId);
-        try
-        {
-            using HttpClient httpClient = httpClientFactory.CreateClient();
-            HttpResponseMessage response = await httpClient.GetAsync(athlete.ImgUrl);
-            contentType = response.Content.Headers.ContentType?.MediaType ?? "image/jpeg";
+        // If content type is not provided, guess based on file extension.
+        contentType ??= athlete.ImgUrl.EndsWith(".png") ? "image/png" : "image/jpeg";
 
-            byte[] data = await response.Content.ReadAsByteArrayAsync(HttpContext.RequestAborted);
-
-            await StoreProfile(athleteId, data, contentType);
-
-            return File(data, contentType);
-        }
-        catch (Exception e)
-        {
-            if (cachedProfile is null)
-            {
-                logger.LogError(e, "Unable to download profile picture, using fallback.");
-
-                return LocalRedirect(PROFILE_FALLBACK);
-            }
-            else
-            {
-                logger.LogError(e, "Unable to download profile picture, using outdated cache.");
-                FileStream profile = cachedProfile.OpenRead();
-
-                return File(profile, contentType);
-            }
-        }
-    }
-
-    private static (FileInfo? file, string contentType) GetCachedProfile(int athleteId)
-    {
-        DirectoryInfo profilesDir = new(PROFILES_DIR);
-        if (!profilesDir.Exists)
-            profilesDir.Create();
-
-        FileInfo? file = profilesDir.EnumerateFiles()
-            .FirstOrDefault(f => f.Name.StartsWith($"{athleteId}."));
-
-        if (file is null)
-            return (null, "");
-
-        if (file.Extension == ".png")
-            return (file, "image/png");
-
-        return (file, "image/jpeg");
-    }
-
-    private static async Task StoreProfile(int athleteId, ReadOnlyMemory<byte> image, string contentType)
-    {
-        string ext = contentType == "image/png" ? "png" : "jpg";
-        string filename = $"{athleteId}.{ext}";
-        string path = Path.Join(PROFILES_DIR, filename);
-
-        await System.IO.File.WriteAllBytesAsync(path, image);
+        return File(data, contentType);
     }
 }
