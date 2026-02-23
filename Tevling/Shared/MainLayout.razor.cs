6100996466a93770823f966143e69acb6b83c05c
Vegard Løkken <vegard@loekken.org>
2026-02-23 22:51:06 +0100

    Scroll to top on navigation
    This is a workaround for Blazor's behavior of keeping scroll position
    when navigating to the same page with different parameters.

--- Tevling/Shared/MainLayout.razor.cs
+++ Tevling/Shared/MainLayout.razor.cs
@@ -1,10 +1,17 @@
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.JSInterop;
+
 namespace Tevling.Shared;
 
-public partial class MainLayout : LayoutComponentBase
+public partial class MainLayout : LayoutComponentBase, IAsyncDisposable
 {
-    [Inject] private IAuthenticationService AuthenticationService { get; set; } = null!;
     [Inject] private Blazored.LocalStorage.ILocalStorageService LocalStorage { get; set; } = null!;
+    [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+    [Inject] private IJSRuntime JsRuntime { get; set; } = null!;
+    private ElementReference MainElement { get; set; }
 
+    private IJSObjectReference? _scrollTopModule;
+    private IDisposable? _locationChangingHandler;
     private string? Theme { get; set; }
     private bool ThemeSet { get; set; }
 
@@ -15,6 +22,20 @@ public partial class MainLayout : LayoutComponentBase
             Theme = await LocalStorage.GetThemeAsync();
             ThemeSet = true;
             StateHasChanged();
+
+            _scrollTopModule = await JsRuntime.InvokeAsync<IJSObjectReference>("import", "./scroll-top.js");
+            _locationChangingHandler = NavigationManager.RegisterLocationChangingHandler(ResetScroll);
+        }
+    }
+
+    private async ValueTask ResetScroll(LocationChangingContext context)
+    {
+        // Scroll to top on internal navigation.
+        // A workaround for Blazor's helpful behavior of keeping scroll position
+        // when navigating to same page with different parameters.
+        if (context.IsNavigationIntercepted && _scrollTopModule != null)
+        {
+            await _scrollTopModule.InvokeVoidAsync("setScrollTop", MainElement, 0);
         }
     }
 
@@ -22,5 +43,26 @@ public partial class MainLayout : LayoutComponentBase
     {
         Theme = newTheme;
         StateHasChanged();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_locationChangingHandler != null)
+        {
+            _locationChangingHandler?.Dispose();
+            _locationChangingHandler = null;
+        }
+
+        if (_scrollTopModule != null)
+        {
+            try
+            {
+                await _scrollTopModule.DisposeAsync();
+            }
+            catch (JSDisconnectedException)
+            {
+            }
+            _scrollTopModule = null;
+        }
     }
 }
---
+++ Tevling/wwwroot/scroll-top.js
@@ -0,0 +1,5 @@
+export function setScrollTop(el, scrollTop) {
+    if (el != null && scrollTop != null) {
+        el.scrollTop = scrollTop;
+    }
+}
